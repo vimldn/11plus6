f464c444c40c188fb7cb56a9880b6a38eb835e7d
Jerwin Manongsong <61395841+nahadgur@users.noreply.github.com>
2026-02-26 12:36:01 +0800

    Update TopicSelectScreen.tsx

--- components/TopicSelectScreen.tsx
+++ components/TopicSelectScreen.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Clock, BookOpen, ArrowLeft, ChevronRight, List, CheckSquare, Type, ArrowDownUp } from 'lucide-react';
